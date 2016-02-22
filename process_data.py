835df227e9b998e99de9aca2c0a1eeebc2e5a552
Christoffer Lofsberg <chris.lofsberg@gmail.com>
2016-02-22 14:43:40 +0100

    Started at isrelevant and add find_direction

--- process_data.py
+++ process_data.py
@@ -1,9 +1,11 @@
-from receiver import Receive
+#from receiver import Receive
+from direction import Direction
+from math import fabs
 
 
 class ProcessData:
 
-    def getAmbulanceData(self):
+    """def getAmbulanceData(self):
         # TODO: Implement listener
 
         # When history stack has two elements
@@ -13,4 +15,43 @@ class ProcessData:
         Receive.history.put(last)  # Put last element back to the history stack
 
         long = Receive.history[long]
-        lat =
+        lat ="""
+
+    def is_relevant(self, new_car, old_car, new_ambu, old_ambu):
+        new_car_pos = (new_car['latitude'], new_car['longitude'])
+        new_car_speed = new_car['speed']
+        new_car_time = new_car['time']
+
+        old_car_pos = (old_car['latitude'], old_car['longitude'])
+        old_car_speed = old_car['speed']
+        old_car_time = old_car['time']
+
+        new_ambu_pos = (new_ambu['latitude'], new_ambu['longitude'])
+        new_ambu_speed = new_ambu['speed']
+        new_ambu_time = new_ambu['time']
+
+        old_ambu_pos = (old_ambu['latitude'], old_ambu['longitude'])
+        old_ambu_speed = old_ambu['speed']
+        old_ambu_time = old_ambu['time']
+
+
+    def _find_direction(self, data1, data2):
+        """Find direction for vehicle, returns Direction enum
+
+        Keyword arguments:
+        data1 -- tuple with latitude and longitude from newest data
+        data2 -- tuple with latitude and longitude from oldest data
+        """
+
+        lat_change = data1[0] - data2[0]
+        long_change = data1[1] - data2[1]
+
+        if lat_change == 0 and long_change == 0:
+            return Direction.standing_still
+        if fabs(lat_change) > fabs(long_change):
+            if lat_change > 0:
+                return Direction.east
+            return Direction.west
+        if long_change > 0:
+            return Direction.north
+        return Direction.south
