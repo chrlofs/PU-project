7cd12b463d6f3f5032172e62c8771c83c55252f6
YumYumNoodle <jonathan.s.linnestad@gmail.com>
2016-02-22 16:01:48 +0100

    Add test for get_ambulance_data

--- process_data_test.py
+++ process_data_test.py
@@ -6,4 +6,10 @@ process = process_data.ProcessData()
 class TestProcessDataMethods(unittest.TestCase): 
 
 	def test_get_ambulance_data(self):
-		self.assert
+		dict1 = {longitude: 1010, altitude: 1010, time: 12123123, speed: 120}
+		dict2 = {longitude: 1010, altitude: 1010, time: 12123123, speed: 120}
+		process.receive.history.put(dict1)
+		process.receive.history.put(dict1)
+		self.assertEquals(process.receive.history.qsize() == 2)
+		self.assertEquals(process.test_get_ambulance_data() == [dict1, dict2])
+		self.assertEquals(process.receive.history.qsize() == 1)
