626f118fb256a71152a991aca4e7bc4f7787610c
Sandra <sandraskarshaug@gmail.com>
2016-02-25 14:38:53 +0100

    Add comments to Sender class

--- Sender.py
+++ Sender.py
@@ -11,23 +11,28 @@ class Sender:
     sock.connect((host, port))
     json_data = []
 
-    def json_list(self, filepath):
-        with open(filepath) as f:
-            index = 0
-            for line in f:
-                j_content = json.loads(line)
+    def json_list(self, file_path):
+        """ Opens file and fills json_data with json objects corresponding to our filter
+
+        :param file_path: file containing json objects
+        """
+
+        with open(file_path) as f:
+            for line in f:  # Loops through lines in file
+                j_content = json.loads(line)  # Deserialize json string
                 if j_content.get('name') == 'longitude' or j_content.get('name') == 'latitude'\
-                        or j_content.get('name') == 'vehicle_speed':
-                            self.json_data.append(j_content)
-                            index += 1
+                        or j_content.get('name') == 'vehicle_speed':  # Filters relevant content
+                            self.json_data.append(j_content)  # Add content to json_data
 
     def send(self):
+        """ Sends json data through socket
+        """
+
         for i in self.json_data:
-            self.sock.send(str(i).encode())
-            time.sleep(0.25)
+            self.sock.send(str(i).encode())  # Encodes string to UTF-8 and sends through socket
+            time.sleep(0.25)  # Waits for 0.25 seconds before sending the next line
 
 if __name__ == "__main__":
     amb = Sender()
     amb.json_list('commute.json')
     amb.send()
-
