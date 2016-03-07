5e082c29b4cd44ff41d5b8d7a769f631683021cc
Sandra <sandraskarshaug@gmail.com>
2016-03-07 13:30:03 +0100

    Add sender.assertEquals

--- sender.py
+++ sender.py
@@ -6,8 +6,8 @@ import json
 class Sender:
 
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    host = '127.0.0.1'  # Receiver's IP-address, must change as needed
-    port = 3000
+    host = '10.22.9.111'  # Receiver's IP-address, must change as needed
+    port = 6001
     sock.connect((host, port))
     json_data = []
 
