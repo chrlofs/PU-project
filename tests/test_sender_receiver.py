ff58df8e57651f70100f762e0a83ab71b983c7fd
Sandra <sandraskarshaug@gmail.com>
2016-03-08 17:01:37 +0100

    Add working test_line_content()

--- tests/test_sender_receiver.py
+++ tests/test_sender_receiver.py
@@ -1,18 +1,15 @@
 import unittest
-import Sender
-import Receiver
+import sender
+import receiver
 import json
+
 
 class TestSenderReceiverMethods(unittest.TestCase):
     """Tests the different methods in receiver and sender class"""
-    receiver = Receiver()
-    sender = Sender()
-    commute_test = ""
 
-    sender.json_list("commute_test.json")
-
-    print(sender.assertEquals(all(p == "longitude" or p == "latitude" or p == "vehicle_speed") for p in sender.json_data["name"]))
-
+    def test_list_content(self):
+        sender.Sender().json_list('commute_test.json')
+        self.assertTrue(all(p['name'] == "longitude" or p['name'] == "latitude" or p['name'] == "vehicle_speed") for p in sender.Sender().json_data)
 
 
 
