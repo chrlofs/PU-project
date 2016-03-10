b9bb7bf13aef601d39be8f9d77299e26bb3dcde4
Alfred <alfredlh@stud.ntnu.no>
2016-03-10 13:18:36 +0100

    Fix documentation on test_sender_receiver.py

--- tests/test_sender_receiver.py
+++ tests/test_sender_receiver.py
@@ -3,16 +3,15 @@ import sender
 import receiver
 import json
 
-
 class TestSenderReceiverMethods(unittest.TestCase):
-    """Tests the different methods in receiver and sender class"""
+    '''Tests the different methods in receiver and sender class'''
 
     def test_list_content(self):
+        '''Tests for valid content in the list'''
+
         sender.Sender().json_list('commute_test.json')
-        self.assertTrue(all(p['name'] == "longitude" or p['name'] == "latitude" or p['name'] == "vehicle_speed") for p in sender.Sender().json_data)
-
-
-
+        self.assertTrue(all(p['name'] == "longitude" or p['name'] == "latitude"\
+            or p['name'] == "vehicle_speed") for p in sender.Sender().json_data)
 
 suite = unittest.TestLoader().loadTestsFromTestCase(TestSenderReceiverMethods)
 unittest.TextTestRunner(verbosity=2).run(suite)
