eef8ebb5328a7ec412d3e33d10202b4af8866b17
Håvard Anda Estensen <haavard.ae@gmail.com>
2016-03-14 12:56:19 +0100

    Add comments

--- car/udp_receiver.py
+++ car/udp_receiver.py
@@ -1,13 +1,16 @@
 import socket
 
+host = ''  # Symbolic name meaning all available interfaces.
 ip = '10.24.7.120'
 port = 10000
 
 # Create a UDP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-sock.bind(("", port))
+# Bind socket to local host and port
+sock.bind((host, port))
 
+# Listen until terminated by user
 while True:
     data, addr = sock.recvfrom(1024)
     data = data.decode(encoding='UTF-8')
