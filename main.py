ba8b74ea0f3e18709f6349b472c87df370f45a84
stianste <stiansteinbakken17@gmail.com>
2016-03-15 12:09:21 +0100

    Fix indents and imports in main.py

--- main.py
+++ main.py
@@ -1,23 +1,22 @@
-from Car.receiver import Receiver
-from Car.process_data import ProcessData
-
+from car.receiver import Receiver
+from car.process_data import ProcessData
 
 class Main:
 
-	def __init__(self):
-		self.receiver = Receiver()
-		self.process_data = ProcessData(self.receiver)
+    def __init__(self):
+        self.receiver = Receiver()
+        self.process_data = ProcessData(self.receiver)
         self.messages = self.process_data.messages
 
-	def run(self):
-		'''Check if receiver has got a new data point, if so process data'''
+    def run(self):
+        '''Check if receiver has got a new data point, if so process data'''
 
-		while self.exit.run:
-			self.receiver.update()
-			if self.receiver.position_history.qsize() >= 2:
-				old_ambu, new_ambu = self.process_data.get_ambulance_data()
-				old_car, new_car = old_ambu, new_car
-				print(self.messages[self.process_data.pick_messages(
+        while self.exit.run:
+            self.receiver.update()
+            if self.receiver.position_history.qsize() >= 2:
+                old_ambu, new_ambu = self.process_data.get_ambulance_data()
+                old_car, new_car = old_ambu, new_car
+                print(self.messages[self.process_data.pick_messages(
                     new_car, old_car, new_ambu, old_ambu)])
 
 main = Main()
