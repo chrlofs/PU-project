93e32808393ab24570138f31cb835cdbbe868eee
Håvard Anda Estensen <haavard.ae@gmail.com>
2016-03-16 23:05:43 +0100

    Add self, comments and remove whitespace

--- ambulance/sender.py
+++ ambulance/sender.py
@@ -5,26 +5,27 @@ import json
 class Sender:
 
     # List of IP addresses to iterate through
-    cars = []
+    self.cars = []
 
     # Add hardcoded IP addresses
-    cars.append('localhost')
-    cars.append('10.24.7.121')
+    self.cars.append('localhost')
+    self.cars.append('10.24.7.121')
 
     # Add starting port
     port = 10000
 
     # Create a UDP socket
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-    json_data = []
+    # JSON objects containing longitude, latitude and speed data
+    self.json_data = []
 
     def json_list(self, file_path):
-        '''Opens file and fills json_data with json
+        '''Open file and fill json_data with JSON
         objects corresponding to our filter
 
         Keyword arguments:
-        file_path -- file containing json objects
+        file_path -- file containing JSON objects
         '''
         with open(file_path) as f:
             for line in f:  # Loops through lines in file
@@ -37,28 +38,24 @@ class Sender:
                             self.json_data.append(j_content)
 
     def send(self):
-        '''Sends json data through socket'''
+        '''Send JSON data to all nearby cars'''
 
         for message in self.json_data:
             # Send message to all the cars on the hardcoded list
-            for car in cars:
-
+            for car in self.cars:
                 # Print for easier debug
                 print(car)
-
                 try:
                     # Send a test message
                     # First convert message to bytes
                     self.sock.sendto(message.encode(encoding='UTF-8'), (car, port))
-
                 except socket.error as e:
                     print('Error code: ' + str(e[0]) + ' Message ' + str(e[1]))
                     sys.exit()
-
             # Waits for 0.25 seconds before sending the next line
             time.sleep(0.25)
 
 if __name__ == "__main__":
     amb = Sender()
     amb.json_list('commute.json')
-    amb.send('10.22.1.188')
+    amb.send()
