b66805a4bbbb211b136d8976392ab8b9fd027df4
Trudelutt <tjostad@hotmail.com>
2016-03-16 19:37:07 +0100

    make main run with two vehicle objects

--- main.py
+++ main.py
@@ -1,24 +1,23 @@
-from Car.receiver import Receiver
-from Car.process_data import ProcessData
+#!/usr/bin/env python3
+from car.vehicle import Vehicle
+from car.process_data import ProcessData
 
 
 class Main:
 
 	def __init__(self):
-		self.receiver = Receiver()
-		self.process_data = ProcessData(self.receiver)
+		self.vechicle1 = Vehicle('reversed')
+		self.vechicle2 = Vehicle()
+		self.process_data = ProcessData(self.vechicle1,self.vechicle2)
 
 	def run(self):
 		'''Check if receiver has got a new data point, if so process data'''
 
-		while self.exit.run:
-			self.receiver.update()
-			if self.receiver.position_history.qsize() >= 2:
-				old_ambu, new_ambu = self.process_data.get_ambulance_data()
-				old_car, new_car = old_ambu, new_car
-				self.process_data.is_relevant(new_car, old_car, new_ambu, old_ambu)
+		while len(self.vechicle1.position_history) >1:
+			old_ambu, new_ambu = self.vechicle1.get_data()
+			old_car, new_car = self.vechicle2.get_data()
+			self.process_data.is_relevant(new_car, old_car, new_ambu, old_ambu)
 
-
-main = Main()
-main.run()
-print(main.receiver.get_stack())
+if __name__ == "__main__":
+	main = Main()
+	main.run()
