6cde5de223667947afa852ef27a1d1347d77cf60
Håvard Anda Estensen <haavard.ae@gmail.com>
2016-04-04 12:48:52 +0200

    Change var names

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3
 from car.vehicle import Vehicle
 from car.process_data import ProcessData
 
@@ -6,18 +5,18 @@ from car.process_data import ProcessData
 class Main:
 
     def __init__(self):
-        self.vechicle2 = Vehicle(speed=2)
-        self.vechicle1 = Vehicle(start_ahead=True)
-        self.process_data = ProcessData(self.vechicle1,self.vechicle2)
+        self.ambulance = Vehicle(speed=2)
+        self.car = Vehicle(start_ahead=True)
+        self.process_data = ProcessData(self.car,self.ambulance)
 
     def run(self):
         '''Check if receiver has got a new data point, if so process data'''
         print('=========== new run ===============')
 
-        while len(self.vechicle1.position_history) > 1 and \
-                len(self.vechicle2.position_history) > 1:
-            old_ambu, new_ambu = self.vechicle1.get_data()
-            old_car, new_car = self.vechicle2.get_data()
+        while len(self.car.position_history) > 1 and \
+                len(self.ambulance.position_history) > 1:
+            old_ambu, new_ambu = self.car.get_data()
+            old_car, new_car = self.ambulance.get_data()
             self.process_data.is_relevant(new_car, old_car, new_ambu, old_ambu)
 
 if __name__ == "__main__":
