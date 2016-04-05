b740e59cee4c64f13b68255b8867f39403590981
Sandra <sandraskarshaug@gmail.com>
2016-04-05 18:20:07 +0200

    make map with mplleaflet

--- car/mapview.py
+++ car/mapview.py
@@ -1,2 +1,37 @@
 import mplleaflet
-mplleaflet.show()
+import json
+import os
+import matplotlib.pyplot as plt
+import numpy as np
+import mplleaflet
+
+class MapView():
+    ''' creates map to show the ambulance and the car '''
+
+
+    def __init__(self):
+        pass
+
+    def plot_coordinates(self, longitude, latitude, color):
+        '''Plot coordinates in map
+        :param longitude:
+        :param latitude:
+        :param color: red or blue for ambulance or car
+        :return:
+        '''
+
+        plt.hold(True)
+        plt.plot(longitude, latitude, color)
+
+
+    def show_map(self):
+        '''Shows map
+        :return:
+        '''
+        
+        mplleaflet.show()
+
+
+if __name__ == "__main__":
+    map = MapView()
+    map.plot_coordinates(-83,40,'rs')
--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 from car.vehicle import Vehicle
 from car.process_data import ProcessData
-from car.GPS_GUI import Carmap
+from car.mapview import MapView
+import gmaps
+
 
 
 class Main:
@@ -10,19 +12,29 @@ class Main:
         self.vechicle1 = Vehicle(start_ahead=True)
         self.vechicle2 = Vehicle(speed=3)
         self.process_data = ProcessData(self.vechicle1,self.vechicle2)
-        self.map = Carmap()
+        self.map = MapView()
+
 
     def run(self):
         '''Check if receiver has got a new data point, if so process data'''
 
+        count = 0
+
         while len(self.vechicle1.position_history) >1 and len(self.vechicle2.position_history) > 1:
             old_ambu, new_ambu = self.vechicle1.get_data()
-            self.map.plot(old_ambu['longitude'], old_ambu['latitude'], 'ro')
+            self.map.plot_coordinates(old_ambu['longitude'],old_ambu['latitude'],'rs')
             old_car, new_car = self.vechicle2.get_data()
-            self.map.plot(old_car['longitude'], old_car['latitude'], 'bo')
+            self.map.plot_coordinates(old_car['longitude'], old_car['latitude'],'bs')
             self.process_data.is_relevant(new_car, old_car, new_ambu, old_ambu)
-        self.map.show()
+        self.map.show_map()
+            # if count == 10:
+            #     print(count)
+            #     count = 0
+            #
+            #
+            # count += 1
 
 if __name__ == "__main__":
     main = Main()
     main.run()
+
