69ad3910f51738b134669453772273ff8517fe6c
YumYumNoodle <jonathan.s.linnestad@gmail.com>
2016-04-07 10:30:04 +0200

    Finish text_to_speech

--- car/text_to_speech.py
+++ car/text_to_speech.py
@@ -1,10 +1,12 @@
 import os
+import time
 
 class TextToSpeech():
 
-	def __init__(self):
-		self.path = 'messages/msg'
+    def __init__(self):
+        self.path = '/support/messages/msg'
 
+    def play(self, message_integer):
+        path = os.path.dirname(os.path.realpath(__file__))
+        os.system('mpg123 play ' + path + self.path + str(message_integer) + '.mp3')
 
-	def play(self, message_integer):
-		os.system('start ' + self.path + str(message_integer))
