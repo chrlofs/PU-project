ce8427ed337102c2751f7f34a4281588ebccdd41
YumYumNoodle <jonathan.s.linnestad@gmail.com>
2016-04-18 14:01:32 +0200

    Add async subprocess to text_to_speech.py

--- car/text_to_speech.py
+++ car/text_to_speech.py
@@ -1,3 +1,4 @@
+import subprocess
 import os
 
 class TextToSpeech():
@@ -8,6 +9,7 @@ class TextToSpeech():
     def play(self, message_integer):
         if message_integer != 0:
             path = os.path.dirname(os.path.realpath(__file__))
-            os.system('mpg123 play ' + path + self.path + str(message_integer) + '.mp3')
+            #os.system('mpg123 play ' + path + self.path + str(message_integer) + '.mp3')
+            subprocess.Popen(['mpg123','play', path + self.path + str(message_integer) + '.mp3'], close_fds=True)
 
 
